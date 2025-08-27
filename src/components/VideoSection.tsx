0d76c42d03632200434d32d9c1c7ed1c43b3febd
bluedropsmagic <admin@magicbluedrops.com>
2025-08-26 23:47:56 -0300

    Updated VideoSection.tsx

--- src/components/VideoSection.tsx
+++ src/components/VideoSection.tsx
@@ -179,3 +179,8 @@ export const VideoSection: React.FC = () => {
               position: 'absolute',
               top: 0, left: 0, width: '100%', height: '100%',
               touc
+
+            }
+            }
+  )
+}
